a63dea953f7e1187b4024f31ad598e630351ee0a
braccnd <punyanyaibra@gmail.com>
2026-07-27 17:00:09 +0800

    membuat footer

---
+++ resources/views/components/home/footer/footer.blade.php
@@ -0,0 +1,50 @@
+<div class="flex flex-col px-16 pt-6 pb-2.5 max-w-360">
+    <div id="top" class="flex justify-between py-4 w-full">
+        <img src={{ asset('images/logo/logo_footer.png') }} alt="logo_footer">
+        <div class="flex gap-16">
+            <div>
+                <h1 class="text-lg font-family-body text-text-primary">Jelajahi</h1>
+                <menu>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Katalog</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Kategori</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Terbaru</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Terlaris</a>
+                    </li>
+                </menu>
+            </div>
+            <div>
+                <h1 class="text-lg font-family-body text-text-primary">Bantuan</h1>
+                <menu>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Kontak</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-lg font-family-body text-text-link hover:underline">Lisensi</a>
+                    </li>
+                </menu>
+            </div>
+        </div>
+    </div>
+    <div id="middle" class="flex justify-between">
+        <h1 class="text-md font-family-body text-text-secondary">&copy; {{ date('Y') }} Red Dev. ALL RIGHT RESERVED
+        </h1>
+        <menu class="flex gap-4">
+            <li><a href="#" class="text-lg font-family-body text-text-primary hover:underline">Terms &
+                    Conditions</a></li>
+            <li><a href="#" class="text-lg font-family-body text-text-primary hover:underline">Privacy Policy</a>
+            </li>
+        </menu>
+    </div>
+    <div id="down" class="font-family-display font-bold text-text-brand text-[260px] text-center tracking-[3px] leading-[0.8]">
+        REDSHOP</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::redirect('/', '/home', 301);
+Route::livewire('/home', 'pages::home')->name('home');
